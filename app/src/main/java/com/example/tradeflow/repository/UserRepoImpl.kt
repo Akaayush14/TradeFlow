4c4ccf7c09d4268b5ac0f2db67b7ee0609953c55
Aayush Kharel <akaayush30@gmail.com>
2025-12-16 21:20:03 +0545

    Intent filter was missing for registerActivity, it is added now. UserRepo, UserRepoImpl, UserViewModel code has been written for successful registartion

--- app/src/main/java/com/example/tradeflow/repository/UserRepoImpl.kt
+++ app/src/main/java/com/example/tradeflow/repository/UserRepoImpl.kt
@@ -1,4 +1,121 @@
 package com.example.tradeflow.repository
 
-class UserRepoImpl {
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.example.tradeflow.model.UserModel
+import kotlin.collections.toMap
+
+class UserRepoImpl: UserRepo{
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    //table haru ma kaam garnu paro vane ref ma garnu paryo
+    val ref: DatabaseReference = database.getReference("Users")
+
+    override fun login(
+        email: String,
+        password: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    callback(true, "login successful")
+                }else{
+                    callback(false, "${it.exception?.message}")
+                }
+            }
+    }
+
+    override fun register(
+        email: String,
+        password: String,
+        callback: (Boolean, String, String) -> Unit
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    callback(true, "Registration success",
+                        "${auth.currentUser?.uid}")
+                }else{
+                    callback(false, "${it.exception?.message}", "")
+                }
+            }
+    }
+
+    override fun addUserToDatabase(
+        userId: String,
+        model: UserModel,
+        callback: (Boolean, String) -> Unit
+    ) {
+        ref.child(userId).setValue(model).addOnCompleteListener {
+            if (it.isSuccessful){
+                callback(true, "Registration successful")
+            }else{
+                callback(true, "${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun forgetPassword(
+        email: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    callback(true, "Link sent to $email")
+                } else {
+                    callback(false, "${it.exception?.message}")
+                }
+            }
+    }
+
+    override fun getCurrentUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+
+    override fun getUserById(
+        userId: String,
+        callback: (Boolean, String, UserModel?) -> Unit
+    ) {
+        ref.child(userId).addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val user=snapshot.getValue(UserModel::class.java)
+                    if (user != null){
+                        callback(true, "Profile Fetched ",user)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                callback(false,error.message,null)
+            }
+        })
+    }
+
+    override fun getAllUser(callback: (Boolean, String, List<UserModel>?) -> Unit) {
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    var allUsers = mutableListOf<UserModel>()
+                    for (data in snapshot.children){
+                        var user = data.getValue(UserModel::class.java)
+                        if(user!=null){
+                            allUsers.add(user)
+                        }
+                    }
+                    callback(true, "User fetched", allUsers)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 }
---
+++ app/src/main/java/com/example/tradeflow/viewmodel/UserViewModel.kt
@@ -0,0 +1,74 @@
+package com.example.classwork.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import com.example.tradeflow.model.UserModel
+import com.example.tradeflow.repository.UserRepo
+
+class UserViewModel(val repo: UserRepo): ViewModel(){
+    fun login(
+        email:String, password:String,
+        callback:(Boolean, String) -> Unit
+    ){
+        repo.login(email, password, callback)
+    }
+
+    fun register(
+        email:String, password: String,
+        callback: (Boolean, String, String) -> Unit
+    ){
+        repo.register(email, password, callback)
+    }
+
+    fun addUserToDatabase(
+        userId: String,
+        model: UserModel, callback: (Boolean, String) -> Unit
+    ){
+        repo.addUserToDatabase(userId, model, callback)
+    }
+
+    fun forgetPassword(
+        email:String,
+        callback:(Boolean, String) -> Unit
+    ){
+        repo.forgetPassword(email, callback)
+    }
+
+    fun getCurrentUser() : FirebaseUser?{
+        return repo.getCurrentUser()
+    }
+
+    private val _users = MutableLiveData<UserModel?>()
+    val users: MutableLiveData<UserModel?> get() = _users
+
+    private val _allUsers = MutableLiveData<List<UserModel>?>()
+
+
+    fun getUserById(
+        userId: String
+    ){
+        repo.getUserById(userId){
+                success,msg,data->
+            if(success){
+                _users.postValue(data)
+            }else{
+                _users.postValue(null)
+            }
+        }
+
+    }
+
+    fun getAllUser()
+    {
+        repo.getAllUser {
+                success, message, data ->
+            if(success){
+                _allUsers.postValue(data)
+            }else{
+                _allUsers.postValue(emptyList())
+            }
+        }
+
+    }
+}
