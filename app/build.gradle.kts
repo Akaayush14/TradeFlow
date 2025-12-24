53f389f2f086fc0d07065a1fe9d18866f61c0e59
Devendra-crate <devendramc6@gmail.com>
2025-12-24 11:19:32 +0545

    ;logo change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.media3:media3-exoplayer:1.2.0")
+    implementation("androidx.media3:media3-ui:1.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/tradeflow/SplashActivity.kt
+++ app/src/main/java/com/example/tradeflow/SplashActivity.kt
@@ -6,17 +6,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tradeflow.ui.theme.TradeFlowTheme
+import com.example.tradeflow.ui.theme.Green1
+import com.example.tradeflow.ui.theme.Greenish
 import kotlinx.coroutines.delay
 
 class SplashActivity : ComponentActivity() {
@@ -36,16 +44,23 @@ fun SplashBody() {
     val activity = context as Activity
 
     LaunchedEffect(Unit) {
-        delay(2000)
-        val intent = Intent(context, LoginUI::class.java)
-    }
+            delay(2000)
+            val intent = Intent(context, LoginActivity::class.java)
+        }
 
     Scaffold { padding ->
         Column (
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
+                .background(Green1),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
+            Image(
+                painter = painterResource(R.drawable.house_rent_logo),
+                contentDescription = null
+            )
 
         }
     }
