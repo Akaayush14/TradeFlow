4b5c51e202c4a4989c8be1fe481e50cf489c6dd7
Devendra-crate <devendramc6@gmail.com>
2026-01-05 11:15:46 +0545

    Small changes in these files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.maps.android:maps-compose:1.0.0")
+    implementation("com.google.android.gms:play-services-maps:18.0.2")
 }
