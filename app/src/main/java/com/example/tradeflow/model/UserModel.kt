1c787e0829bad13d291e26f37ee9b7fc8b497f9a
Aayush Kharel <akaayush30@gmail.com>
2026-01-11 12:26:35 +0545

    Added new fields that are present in UserSettingEditProfile

--- app/src/main/java/com/example/tradeflow/model/UserModel.kt
+++ app/src/main/java/com/example/tradeflow/model/UserModel.kt
@@ -5,6 +5,10 @@ data class UserModel(
     var name: String = "",
     var email: String = "",
     var phone: String = "",
+    var gender: String = "",           // NEW
+    var dob: String = "",              // NEW (Date of Birth)
+    var location: String = "",         // NEW (Country)
+    var profilePhotoUrl: String = "",  // NEW (Optional - for future use)
     var isBlocked: Boolean = false,
     var isRestricted: Boolean = false,
     var points: Long = 0L
@@ -15,6 +19,10 @@ data class UserModel(
             "name" to name,
             "email" to email,
             "phone" to phone,
+            "gender" to gender,           // NEW
+            "dob" to dob,                 // NEW
+            "location" to location,       // NEW
+            "profilePhotoUrl" to profilePhotoUrl, // NEW
             "isBlocked" to isBlocked,
             "isRestricted" to isRestricted,
             "points" to points
