33566dce61fdbf3afd9e8bec8ce94543a2b0305f
Sidhartha443 <lucisah3@gmail.com>
2026-01-14 11:10:43 +0545

    adduserpointdeal in point dealmodel

--- app/src/main/java/com/example/tradeflow/model/PointDealModel.kt
+++ app/src/main/java/com/example/tradeflow/model/PointDealModel.kt
@@ -47,5 +47,27 @@ data class PointDealModel(
         )
     }
 
+    data class PointRedemptionModel(
+        var redemptionId: String = "",
+        var userId: String = "",
+        var dealId: String = "",
+        var pointsSpent: Long = 0L,
+        var redeemedAt: Long = System.currentTimeMillis(),
+        var dealTitle: String = "",
+        var dealOffer: String = ""
+    ) {
+        fun toMap(): Map<String, Any?> {
+            return mapOf(
+                "redemptionId" to redemptionId,
+                "userId" to userId,
+                "dealId" to dealId,
+                "pointsSpent" to pointsSpent,
+                "redeemedAt" to redeemedAt,
+                "dealTitle" to dealTitle,
+                "dealOffer" to dealOffer
+            )
+        }
+    }
+
 }
 
